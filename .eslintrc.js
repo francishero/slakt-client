6fdc2ac2de64f5b035d0108aa3a5bc217ea3518c
Francis Miyoba <francismiyoba@gmail.com>
2017-12-05 05:59:00 -0800

    Setup client

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "semi": "off",
+        "react/jsx-filename-extension": "off"
+    },
+    "globals" : {
+        "document" : 1
+    }
+};
