b5630a13836333293647486db2c9f18a9e2db08a
Francis Miyoba <francismiyoba@gmail.com>
2018-01-03 06:53:42 -0800

    Can create channels and switch to them

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -53,6 +53,7 @@ class Sidebar extends React.Component {
         <Channels
           key="channels-sidebar"
           teamName={team.name}
+          teamId={team.id}
           username={username}
           channels={team.channels}
           users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
